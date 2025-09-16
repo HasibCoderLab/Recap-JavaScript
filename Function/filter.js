4b440288cef9739646512248567c970b2574af49
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-09-16 23:48:14 +0600

    solve

--- Function/filter.js
+++ Function/filter.js
@@ -1,7 +1,13 @@
 // ================================================
 //               Language: JavaScript
 //               Topic   : [Function]
-//               Problem :👉👉 [7]
+//               Problem :👉👉 [8]
 //               Want    :  filter
 //               file    : filter.js
 // ================================================
+let num = [1,12,24,20,,35,25,3];
+let newNum = num.filter((val) =>{
+   return  val %2 === 0 ;
+   
+})
+console.log(newNum);
