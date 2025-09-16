ba1495020caeae0cd47675805c5b6947d234ac44
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-09-16 07:46:11 +0600

    first commit

---
+++ concatforString.js
@@ -0,0 +1,13 @@
+// ================================================
+//               Language: JavaScript
+//               Topic   : [ForOfLoop]
+//               Problem :👉👉 [4]
+//               Want    :  concat()
+//               file    : concatforString.js             
+// ================================================
+let BoysName = ["Joy" , "Arif" , "Mizan" , "Mahim"];
+let GirlsName = ["Ankhi" , "Mahi" , "Habiba" , "Mahima"];
+let Teachers = ["Shuvom" ,"Manik" , "Arafat" , "Riab"];
+
+let Names   = BoysName.concat(GirlsName,Teachers);
+console.log(Names);
---
+++ toString.js
@@ -0,0 +1,9 @@
+// ================================================
+//               Language: JavaScript
+//               Topic   : [ForOfLoop]
+//               Problem :👉👉 [3]
+//               Want    : ToString
+//               file    : toString.js
+// ================================================
+let name = ["Coder" , "Editor", "Poet", "Writer"];
+console.log(name.toString());
