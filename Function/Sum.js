3c4e066c0496ddf5c464eab54c21135cab1cf525
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-09-16 08:48:24 +0600

    solve

--- Function/Sum.js
+++ Function/Sum.js
@@ -5,3 +5,12 @@
 //               Want    :  sum2Number
 //               file    : Sum.js             
 // ================================================
+function sum(a ,b) {
+    return a + b;
+
+    
+}
+
+const result = sum(20 , 3);
+console.log(` Sum is ${result}`);
+
