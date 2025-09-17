24ed6e3f7fa8b0da97b41978f5cbc1688bd67100
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-09-17 22:45:31 +0600

    solve

---
+++ ProblemSolving/UseFilter.js
@@ -0,0 +1,12 @@
+// ================================================
+//               Language: JavaScript
+//               Topic   : [Function]
+//               Problem :👉👉 [10]
+//               Want    :  LargestReduce
+//               file    : Node ProblemSolving/UseFilter.js
+// ================================================
+let arr  = [ 10 , 55,98, 98, 95, 78,65 ];
+newArr = arr.filter((value) => {
+    return value > 90;
+})
+console.log(newArr);
