10537b4137db863e133743f1c9d3ef6989abcb76
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-09-17 23:03:29 +0600

    solve

--- GivenNumByUser.js
+++ GivenNumByUser.js
@@ -5,3 +5,15 @@
 //               Want    :  GivenNumByUser.js
 //               file    : Node ProblemSolving/GivenNumByUser.js
 // ================================================
+
+// let n =  prompt("Enter Num") ;
+
+let n = 5
+
+let arr = [] ;
+// let arr = [] ;
+for(let i = 0 ; i<= n ; i++){
+    arr[i]  = i;
+}
+console.log(arr);
+
