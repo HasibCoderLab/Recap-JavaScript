d6c17b93a72aa171757dfa574a9332dd5a06026f
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-09-19 12:24:09 +0600

    solve

--- ProblemSolving/square-numbers-map.js
+++ ProblemSolving/square-numbers-map.js
@@ -3,6 +3,14 @@
 //               Topic   : [Solve Problem]
 //               Problem :👉👉 [13]
 //               Want    :  square-numbers-map
-//               file    : Node ProblemSolving/square-numbers-map.js
+//               file    : node ProblemSolving/square-numbers-map.js
 // ================================================
+let num = [2,3,4,5,7,6,]
+
+
+const output = num.map((value) =>{
+ return value * value
+})
+console.log(output);
+
 
